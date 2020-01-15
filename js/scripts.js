0f410c465f3c9a983c20633ab19ec21bbbfc1f1e
Student <example@example.com>
2020-01-15 09:57:46 -0800

    add all working files for form letter

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+  $("#form").submit(function (event) {
+    var nameInput = $("input#name").val();
+    $(".name").text(nameInput);
+
+
+    $("#letter").show();
+
+    event.preventDefault();
+  });
+});
